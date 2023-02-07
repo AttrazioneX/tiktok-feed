3de3031ba6f978400cfc6be5148aba21a39c90e8
AttrazioneX <124664675+AttrazioneX@users.noreply.github.com>
2023-02-07 10:30:30 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -6,10 +6,10 @@ from datetime import datetime, timezone
 # Now using a new TikTok library https://github.com/Russell-Newton/TikTokPy
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://github.com/AttrazioneX/tiktok-rss-flat"
+# ghPagesURL = "https://attrazionex.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://github.com/AttrazioneX/tiktok-rss-flat"
+ghPagesURL = "https://attrazionex.github.io/tiktok-rss-flat/"
 
 maxItems = 5
 
